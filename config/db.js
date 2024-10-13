734a7fee736a7a7817814a05ebb92b10d1a673d3
Sufairath C <sufairath.c@gmail.com>
2024-10-13 19:26:34 +0400

    updation

--- config/db.js
+++ config/db.js
@@ -15,3 +15,22 @@ const connect = async () => {
 };
 
 export default connect;
+
+
+// env
+// DB_URL='mongodb+srv://sufairathc:VlZ8M4dvEv4j9Nvj@cluster0.ndlxe7f.mongodb.net/'
+// SECRET_KEY='sufaira8372'
+// CLOUDINARY_CLOUD_NAME='dfxjs9nwa'
+// CLOUDINARY_API_KEY='489195535428673'
+// CLOUDINARY_API_SECRET='8AIP9wq-8Jfh2MEBkNhm9OLWzNE'
+
+
+// # EMAIL_USER='sufairath.c@gmail.com'
+// # EMAIL_PASS='gtnu cdnm zeil ymwz'
+
+// RECAPTCHA_SECRET_KEY=6Ld8KwYqAAAAAPTLSjgQBmCwkE1UPMfeBSTO7HjW
+
+
+
+// EMAIL_USER='emiratesdrive774@gmail.com'
+// EMAIL_PASSWORD='jexk zkff kqti iixb'
